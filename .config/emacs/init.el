75dd9f5d0c3ea2ed90386a43f366977c97a2cf8a
fioriandrea <andrea.fiori.1998@gmail.com>
2025-03-20 19:10:21 +0100

    refactor init.el in more use-package sections

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,30 +7,8 @@
   (find-file user-init-file))
 
 (defun tick (cmd)
-  ;; (interactive "M")
-  ;; (find-file (tick "which ricegit"))
   (let ((output (shell-command-to-string cmd)))
     (string-trim output)))
-
-(defun ctags-file-create (dir-name)
-  "Create tags file."
-  (interactive "DDirectory: ")
-  (shell-command
-   ;; -e for emacs format
-   (format "%s -f TAGS -e -R %s" "ctags" (directory-file-name dir-name))))
-
-(defun close-all-buffers ()
-  "Kill all buffers without regard for their origin."
-  (interactive)
-  (mapc 'kill-buffer (buffer-list)))
-
-(defun close-all-other-buffers ()
-  "Kill all buffers except current buffer."
-  (interactive)
-  (let* ((predicate (lambda (b) (not (eq b (current-buffer)))))
-	     (buffers (seq-filter predicate (buffer-list))))
-    (mapc 'kill-buffer buffers)
-    (delete-other-windows)))
 
 (defconst emacs-backup-dir
   (file-name-as-directory
@@ -45,9 +23,14 @@
 (when (file-exists-p proxy-file)
   (load proxy-file))
 
-(use-package package
-  :defer t
+(use-package use-package
   :ensure nil
+  :custom
+  (use-package-always-ensure nil)
+  (use-package-always-demand nil)
+  (use-package-always-defer t))
+
+(use-package package
   :config
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")))
 
@@ -55,23 +38,9 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; https://elpa.gnu.org/devel/doc/use-package.html#The-emacs-package
 (use-package emacs
-  :defer t
-  :ensure nil
-  :bind
-  ("M-/" . hippie-expand)
   :custom
-  (hippie-expand-try-functions-list '(try-complete-file-name
-                                      try-expand-dabbrev
-                                      try-expand-dabbrev-all-buffers
-                                      try-expand-dabbrev-from-kill))
-  ;; (pop-up-windows nil)
-  ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Choosing-Window-Options.html#index-pop_002dup_002dwindows_002c-replacement-for
-  (display-buffer-base-action
-   '((display-buffer-reuse-window
-      display-buffer-same-window
-      display-buffer-in-previous-window
-      display-buffer-use-some-window)))
   (help-window-select t)
   (global-display-line-numbers-mode t)
   (ring-bell-function 'ignore)
@@ -93,7 +62,21 @@
   (auto-save-list-file-prefix (file-name-as-directory emacs-autosave-dir))
   (auto-save-file-name-transforms `((".*" ,emacs-autosave-dir t)))
   (backup-directory-alist `(("." . ,emacs-backup-dir)))
-  ;; Auto-revert
+  :init
+  (add-to-list 'default-frame-alist '(fullscreen . maximized)))
+
+(use-package window
+  :custom
+  ;; (pop-up-windows nil)
+  ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Choosing-Window-Options.html#index-pop_002dup_002dwindows_002c-replacement-for
+  (display-buffer-base-action
+   '((display-buffer-reuse-window
+      display-buffer-same-window
+      display-buffer-in-previous-window
+      display-buffer-use-some-window))))
+
+(use-package autorevert
+  :custom
   (global-auto-revert-mode t)
   (auto-revert-verbose nil)
   (global-auto-revert-non-file-buffers t)
@@ -102,21 +85,31 @@
   ;; https://lists.gnu.org/archive/html/emacs-devel/2014-10/msg00743.html
   ;; https://emacs.stackexchange.com/a/50134 (read comments)
   (auto-revert-interval 5)
-  (global-auto-revert-ignore-modes '(Buffer-menu-mode))
-  :init
-  (add-to-list 'default-frame-alist '(fullscreen . maximized))
+  (global-auto-revert-ignore-modes '(Buffer-menu-mode)))
+
+(use-package windmove
+  :custom
   (windmove-default-keybindings)
   (windmove-swap-states-default-keybindings '(shift control)))
 
+(use-package hippie-exp
+  :bind
+  ("M-/" . hippie-expand)
+  :custom
+  (hippie-expand-try-functions-list '(try-complete-file-name
+                                      try-expand-dabbrev
+                                      try-expand-dabbrev-all-buffers
+                                      try-expand-dabbrev-from-kill)))
+
 (use-package tab-bar
-  :defer t
-  :ensure nil
   :custom
   (tab-bar-show 1))
 
+(use-package recentf
+  :custom
+  (recentf-mode t))
+
 (use-package tramp
-  :defer t
-  :demand nil
   :custom
   (auto-revert-remote-files nil)
   ;; https://www.gnu.org/software/emacs/manual/html_node/tramp/Auto_002dsave-File-Lock-and-Backup.html
@@ -135,8 +128,6 @@
 	   tramp-file-name-regexp)))
 
 (use-package icomplete
-  :ensure nil
-  :defer t
   :custom
   ;; (tab-always-indent 'complete)
   ;; (completion-auto-select 'second-tab)
@@ -144,14 +135,10 @@
   (fido-vertical-mode t))
 
 (use-package eldoc
-  :defer t
-  :ensure nil
   :custom
   (eldoc-echo-area-use-multiline-p nil))
 
 (use-package org
-  :defer t
-  :ensure nil
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c c" . org-capture)
@@ -162,12 +149,10 @@
   (org-html-validation-link nil))
 
 (use-package org-tempo
-  :after org
-  :ensure nil)
+  :demand t
+  :after org)
 
 (use-package dired
-  :defer t
-  :ensure nil
   :hook
   (dired-mode . dired-hide-details-mode)
   :custom
@@ -178,12 +163,13 @@
 
 (use-package dired-x
   :demand t
-  :after dired
-  :ensure nil)
+  :after dired)
 
 (use-package magit
-  :ensure nil
-  :defer t
+  :if (package-installed-p 'magit)
+  :bind
+  ("C-x g" . magit-status)
+  ("C-x p m" . magit-project-status)
   :hook
   (magit-diff-mode . (lambda () (setq truncate-lines nil)))
   (magit-status-mode . (lambda () (setq truncate-lines nil)))
@@ -193,8 +179,7 @@
   (magit-auto-revert-mode nil))
 
 (use-package dired-subtree
-  :ensure nil
-  :defer t
+  :if (package-installed-p 'dired-subtree)
   :after dired
   :bind
   (:map dired-mode-map
