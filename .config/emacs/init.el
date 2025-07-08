22eae972ffacdf3190ef065b24738bf7d468302e
fioriandrea <andrea.fiori.1998@gmail.com>
2025-07-08 21:41:33 +0200

    init.el: completion-auto-help t

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -506,7 +506,7 @@ auto-revert. Taken from doomemacs."
   (completion-auto-select nil)
   (completions-max-height 12)
   (completion-show-help nil)
-  (completion-auto-help 'visible)
+  (completion-auto-help t)
   (suggest-key-bindings t)
   (completions-detailed nil))
 
