caa07e9724e9b54dcb5975d74b5ca36fb103a470
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-04-03 22:40:07 +0200

    xinitrc - add xfce4-screensaver to xfce example

--- .config/X11/xinitrc.d/examples/10-xfce4-stuff.sh
+++ .config/X11/xinitrc.d/examples/10-xfce4-stuff.sh
@@ -3,4 +3,5 @@
 pgrep -x xfce4-panel || xfce4-panel &
 pgrep -x xfce4-power-manager || xfce4-power-manager &
 pgrep -x xfsettingsd || xfsettingsd &
+pgrep -x xfce4-screensaver || xfce4-screensaver &
 # pgrep -x xfdesktop || xfdesktop &
