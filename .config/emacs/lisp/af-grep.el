68f1ed8ad2d7263466889b6f3069f8d275a7d01e
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-04-03 22:40:03 +0200

    init.el and af-grep.el - add full copyright notice

--- .config/emacs/lisp/af-grep.el
+++ .config/emacs/lisp/af-grep.el
@@ -1,14 +1,26 @@
 ;;; af-grep.el --- Grep-like functionality in pure elisp -*- lexical-binding: t; -*-
 
 ;; Copyright (C) Andrea Fiori
-;; This file is NOT part of GNU Emacs.
-;; This file is free software.
 
 ;; Author: Andrea Fiori
 ;; Package-Version: 20251812.0756
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience, extensions, files, matching, tools, unix
-;; License: GPLv3
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
