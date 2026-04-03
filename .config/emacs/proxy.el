80713eae7398f392f88aae22ba1b05c7064e8798
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-04-03 22:40:06 +0200

    proxy.el - remove comments

--- .config/emacs/proxy.el
+++ .config/emacs/proxy.el
@@ -1,12 +1,12 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; ;; https://stackoverflow.com/questions/10787087/use-elpa-emacs-behind-a-proxy-requiring-authentication
-;; (setq url-proxy-services
-;;       '(("no_proxy" . "127.0.0.1,*.local,otherhost...")
-;;         ("http" . "proxy.com:8080")
-;;         ("https" . "proxy.com:8080")))
+(setq url-proxy-services
+      '(("no_proxy" . "127.0.0.1,*.local,otherhost...")
+        ("http" . "proxy.com:8080")
+        ("https" . "proxy.com:8080")))
 
-;; (setq url-http-proxy-basic-auth-storage
-;;       (list (list "proxy.com:8080"
-;;                   (cons "Input your LDAP UID !"
-;;                         (base64-encode-string "LOGIN:PASSWORD")))))
+(setq url-http-proxy-basic-auth-storage
+      (list (list "proxy.com:8080"
+                  (cons "Input your LDAP UID !"
+                        (base64-encode-string "LOGIN:PASSWORD")))))
