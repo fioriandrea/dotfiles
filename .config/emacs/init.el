ebb68a3aacc3e5790f3b3b88e96ba22aba9ecfbf
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-04-03 22:40:02 +0200

    init.el - add global require xref to grep section

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -112,6 +112,8 @@
 
 ;;;;; Grep
 
+(require 'xref)
+
 (defun my-grep-files (files regexp)
   (require 'cl-lib)
   (let ((results nil))
@@ -186,7 +188,6 @@
                                       len))))))
 
 (defun my-grep-xrefs-show (regexp files)
-  (require 'xref)
   (let ((fetcher (lambda (regexp files)
                    (unless files
                      (user-error "Empty file list"))
