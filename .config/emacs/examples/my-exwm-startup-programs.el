7b6b2106383694be93d8fba2f9ef0c0ecc27b7cf
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-04-05 08:48:41 +0200

    exwm - don't let emacs manage startup programs subprocesses
    https://emacs.stackexchange.com/questions/22363/possible-to-detach-a-child-process

--- .config/emacs/examples/my-exwm-startup-programs.el
+++ .config/emacs/examples/my-exwm-startup-programs.el
@@ -2,10 +2,10 @@
 
 (defun my-exwm-startup-programs ()
   (dolist (cmd '("compton --dbus"
-               "xfce4-power-manager"
-               "xfce4-volumed-pulse"
-               "nm-applet"
-               "redshift-gtk"))
-    (start-process-shell-command cmd nil cmd)))
+                 "xfce4-power-manager"
+                 "xfce4-volumed-pulse"
+                 "nm-applet"
+                 "redshift-gtk"))
+    (call-process-shell-command cmd nil 0 nil)))
 
 (add-hook 'exwm-init-hook 'my-exwm-startup-programs)
