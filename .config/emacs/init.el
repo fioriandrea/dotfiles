4b082f9dd70b90fd975de2b56ab8af8e68d86287
Andrea Fiori <andrea.fiori.1998@gmail.com>
2026-08-04 17:44:46 +0200

    init.el - enable org-use-speed-commands

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -492,6 +492,7 @@ Expands only when PACK is loadable, and reports setup errors."
   (org-startup-truncated nil)
   (org-html-validation-link nil)
   (org-edit-src-content-indentation 0)
+  (org-use-speed-commands t)
   :config
   (setf (alist-get 'file org-link-frame-setup) 'find-file))
 
